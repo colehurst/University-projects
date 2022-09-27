68fea515148adc9662475a9d6ab5bffb3c4dfc46
cole hurst <colehurst97@gmail.com>
2022-09-27 14:14:23 -0500

    old laptop old code push
    combining all my code to make it look pretty

---
+++ everything/Project2/Dice-War-master/cmake-build-debug/CMakeFiles/DiceWar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiceWar.dir/main.cpp.o"
+  "DiceWar.exe"
+  "DiceWar.pdb"
+  "libDiceWar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiceWar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
